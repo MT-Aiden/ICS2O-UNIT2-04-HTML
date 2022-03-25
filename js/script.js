56eb756ec1ac18e4e0f2478faf5d694c413fd990
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-03-25 16:39:39 +0000

    curtis helped me he is so smart

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aiden McLeod
+// Created on: Mar 2022
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
-    // this function displays "Hello, World!"
-    document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
-  }
-  
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const base = parseInt(document.getElementById('base').value)
+  const height = parseInt(document.getElementById('height').value)
+
+  // process
+  const area = (base * height) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
